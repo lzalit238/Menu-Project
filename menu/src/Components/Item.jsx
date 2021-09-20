ecd63109e80d68bdee9f3492f73fd02fada46065
lzalit238 <lzalit238@west-mec.org>
2021-09-20 15:31:04 -0700

    Create Project

---
+++ menu/src/Components/Item.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import data from './data.js'
+
+const Item = () => {
+    return (
+        <>
+            <div key={id}>
+                
+            </div>
+        </>
+    )
+}
+
+export default Item
